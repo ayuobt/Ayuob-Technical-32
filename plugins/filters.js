1b35f39e2eac4a51b9c04cf9ff2540eb034d99d1
itxammy <54495938+itxammy@users.noreply.github.com>
2021-09-10 15:49:11 +0500

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -150,7 +150,7 @@ Asena.addCommand({on: 'text', fromMe: false}, (async (message, match) => {
     if (!!message.mention && message.mention[0] == '919946432377@s.whatsapp.net') {
 await message.client.sendMessage(message.jid, fs.readFileSync('./sticker/song.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted : message.data, ptt: false})
     }
-const array = ['Pikachu','Msd','Vijay','Rashmika','song','Sry','Nikal','Aarulle','achodaa','ayin','Aysheri','Ayye','Ayyo','broken','bye','chattho','cute','Da','Eee','Eee2','engane und','Entha','Enthada','Girls','Good morning','Good night','Hi','Hy','ithokke enth','ivan','Kurippe','Kurumb','Love','Mm','naanam','nadakkatte','Ok','paavam','Pattumo','pikachu','Pm','poda','Pova','Save','Sed','setth','sho','Shoo','Smile','tag','Udayipp','umma','Vaa','Vannu','yo','ys' ]
+const array = ['Pikachu','Msd','Vijay','Rashmika','song','Sry','Nikal','Dance','Happy','Hlo','Aysheri','Like','Ayyo','broken','bye','chattho','cute','Da','Eee','Eee2','Police','angry','aysheri','Girls','Good morning','Good night','Hi','Hy','ithokke enth','ivan','cry','Kurumb','Love','Mm','danc','dead','Ok','paavam','die','pikachu','Pm','dost','eda','Save','Sed','setth','eh','Shoo','Smile','tag','girl','umma','hy','Vannu','yo','ys','important','inbox','johnny','kee','love','madthu','mention','mood','ok','ooo','pever','sad life','saved','sed','shut','teach','think','uff','vibe' ]
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
