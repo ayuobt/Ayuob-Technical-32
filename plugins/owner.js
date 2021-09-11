6997456c99c962b09947a68b9df335ca615d9dd2
itxammy <54495938+itxammy@users.noreply.github.com>
2021-09-11 23:19:50 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -25,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Pikachu Bot created by *Ameer Suhail*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Pikachu Bot created by *Baala*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.PLK + '\n\n---------------------', MessageType.text);
@@ -43,10 +43,10 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Bot created by *Baala*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'Pikachu Bot created by *Baala*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '\n\n--------------------', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
         }
     }));
 }
