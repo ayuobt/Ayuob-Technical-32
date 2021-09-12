7eddc0e4e276c201266e1636757fe50c89bca268
itxammy <54495938+itxammy@users.noreply.github.com>
2021-09-12 16:05:00 +0500

    Update neon.js

--- plugins/neon.js
+++ plugins/neon.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/bneon?apikey=LVJsk3tIlDiQUa4AuncQAYe9XIp&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '```Made by Luttappi🧞```' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '```Made by Baala🧞```' })
 
     }));
 }
