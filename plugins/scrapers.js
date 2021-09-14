ae95d8d6d8fa9ac0aef89fb2c34c7a715db29b45
itxammy <54495938+itxammy@users.noreply.github.com>
2021-09-14 22:47:07 +0500

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -550,9 +550,35 @@ Asena.addCommand({pattern: 'song ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (a
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4, quoted: message.data, caption: Config.CAP, thumbnail: thumb});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4});
         });
     }));
+
+    Asena.applyCMD({ pattern: 'apk ?(.*)', fromMe: LOL, desc: Lang.APK_DESC,  deleteCommand: false}, async (message, match) => {
+
+    const pack = match[1]
+          
+    if (!pack) return await message.client.sendMessage(message.jid,Lang.APK_NEED,MessageType.text, {quoted: message.data})
+
+    if (pack.includes('=')) {
+        var split = pack.split('=');
+        link = split[1];
+        nothing = split[0];
+    }
+    
+    var apk = await WhatsAsena.apk(link)
+    var apkinfo = await WhatsAsena.apk_info(link)
+
+    const app = await axios.get(apkinfo.ic, {responseType: 'arraybuffer'})
+    await message.client.sendMessage (message.jid, Buffer.from (app.data), MessageType.image, {mimetype: Mimetype.png, caption: Lang.APK_D + Lang.APK_N + apk.name + Lang.APK_DEV + apk.auth + Lang.APK_V + apk.vers + Lang.APK_SUM + apkinfo.summ })
+
+    var downloading = await message.client.sendMessage(message.jid,Lang.APK_DW,MessageType.text, {quoted: message.data});
+    const profileBuffer = await axios.get(apk.link, {responseType: 'arraybuffer'})
+    var uploading = await message.client.sendMessage(message.jid,Lang.APK_UP,MessageType.text, {quoted: message.data});
+    await message.client.sendMessage(message.jid,Buffer.from(profileBuffer.data), MessageType.document, {filename: apk.name + '.apk', mimetype: 'application/vnd.android.package-archive', quoted: message.data})
+    await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    return await message.client.deleteMessage(message.jid, {id: uploading.key.id, remoteJid: message.jid, fromMe: true})
+})
 
     Asena.addCommand({pattern: 'yt ?(.*)', fromMe: true, desc: Lang.YT_DESC}, (async (message, match) => { 
 
@@ -1027,7 +1053,7 @@ Asena.addCommand({pattern: 'song ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (a
             });
     }));
 
-    Asena.addCommand({pattern: 'video ?(.*)', fromMe: false, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
+    Asena.addCommand({pattern: 'video ?(.*)', fromMe: true, desc: Lang.VIDEO_DESC}, (async (message, match) => { 
 
         if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_VIDEO,MessageType.text);    
     
@@ -1050,9 +1076,35 @@ Asena.addCommand({pattern: 'song ?(.*)', fromMe: true, desc: Lang.SONG_DESC}, (a
 
         yt.on('end', async () => {
             reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_VIDEO,MessageType.text);
-            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4, quoted: message.data, caption: Config.CAP, thumbnail: thumb});
+            await message.client.sendMessage(message.jid,fs.readFileSync('./' + VID + '.mp4'), MessageType.video, {mimetype: Mimetype.mp4});
         });
     }));
+
+Asena.applyCMD({ pattern: 'apk ?(.*)', fromMe: LOL, desc: Lang.APK_DESC,  deleteCommand: false}, async (message, match) => {
+
+    const pack = match[1]
+          
+    if (!pack) return await message.client.sendMessage(message.jid,Lang.APK_NEED,MessageType.text, {quoted: message.data})
+
+    if (pack.includes('=')) {
+        var split = pack.split('=');
+        link = split[1];
+        nothing = split[0];
+    }
+    
+    var apk = await WhatsAsena.apk(link)
+    var apkinfo = await WhatsAsena.apk_info(link)
+
+    const app = await axios.get(apkinfo.ic, {responseType: 'arraybuffer'})
+    await message.client.sendMessage (message.jid, Buffer.from (app.data), MessageType.image, {mimetype: Mimetype.png, caption: Lang.APK_D + Lang.APK_N + apk.name + Lang.APK_DEV + apk.auth + Lang.APK_V + apk.vers + Lang.APK_SUM + apkinfo.summ })
+
+    var downloading = await message.client.sendMessage(message.jid,Lang.APK_DW,MessageType.text, {quoted: message.data});
+    const profileBuffer = await axios.get(apk.link, {responseType: 'arraybuffer'})
+    var uploading = await message.client.sendMessage(message.jid,Lang.APK_UP,MessageType.text, {quoted: message.data});
+    await message.client.sendMessage(message.jid,Buffer.from(profileBuffer.data), MessageType.document, {filename: apk.name + '.apk', mimetype: 'application/vnd.android.package-archive', quoted: message.data})
+    await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    return await message.client.deleteMessage(message.jid, {id: uploading.key.id, remoteJid: message.jid, fromMe: true})
+})
 
     Asena.addCommand({pattern: 'yt ?(.*)', fromMe: false, desc: Lang.YT_DESC}, (async (message, match) => { 
 
