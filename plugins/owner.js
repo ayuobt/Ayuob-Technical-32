bc8ddaffdcb93ced9dfe2f00e873032f95a5c7dc
itxammy <54495938+itxammy@users.noreply.github.com>
2021-09-15 01:08:12 +0500

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -18,14 +18,14 @@ Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (messa
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/06/Dark-Knight-Game-Photos-free-computer-desktop.jpg";
+  r_text[0] = "https://media4.giphy.com/media/XfzD4PfNXCrNvZ4rAy/giphy.webp?cid=6c09b952e075f6c5de8f09a3319632da4d497eac9337f871&rid=giphy.webp&ct=g";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Baala*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.gif, {mimetype: Mimetype.gif, caption: `*Baala*
 owner name : *Baala*
 Partners : YasirSial : EvilZeemi *
 *Youtube : https://youtube.com/channel/UCukP4xTkzkJh_XOaLXZ4SZA
