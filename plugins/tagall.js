04833a8c5102819f73ded046de914b33f39eec54
itxammy <54495938+itxammy@users.noreply.github.com>
2021-09-17 13:13:28 +0500

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -177,35 +177,4 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     }
 }));
     
-     Asena.addCommand({pattern: 'report ?(.*)', fromMe: false, desc: 'to report someone'}, (async (message, match) => {
-        if (match[1] == '') {
-            let grup = await message.client.groupMetadata(message.jid);
-            var jids = [];
-            mesaj = '';
-            grup['participants'].map(async (uye) => {
-                if (uye.isAdmin) {
-                    mesaj += '@' + uye.id.split('@')[0] + ' ';
-                    jids.push(uye.jid);
-                }
-            });
-           await message.client.sendMessage(message.jid,'ℝ𝔼ℙ𝕆ℝ𝕋 \n'+ mesaj , MessageType.extendedText, {quoted: message.data, contextInfo: {mentionedJid: jids}, previewType: 0})
-        
-        }
-        else if (match[1] !== '' && message.reply_message) {
-            let grup = await message.client.groupMetadata(message.jid);
-            var jids = [];
-            mesaj = '';
-            grup['participants'].map(async (uye) => {
-                if (uye.isAdmin) {
-                    mesaj += '@' + uye.id.split('@')[0] + ' ';
-                    jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
-                }
-            });
-            await message.client.sendMessage(message.jid,'ℝ𝔼ℙ𝕆ℝ𝕋\n' + mesaj  + '\n\nℝ𝔼𝔸𝕊𝕆ℕ :  ' + `${match[1]}`,  MessageType.extendedText, {quoted: message.data, contextInfo: {mentionedJid: jids}, previewType: 0})
-        }
-        else if (!message.reply_message) {
-            return message.client.sendMessage(message.jid,'Please Respond to Users Message to Report', MessageType.text);
-        }
-}));
-
 }
